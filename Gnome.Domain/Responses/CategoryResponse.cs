2861d81f00c601104fa64f5db6d209810b2550a0
percuklieski <percuklieski@gmail.com>
2025-06-19 16:32:56 +0200

    Responses for single database hit (by id)

---
+++ Gnome.Domain/Responses/CategoryResponse.cs
@@ -0,0 +1,33 @@
+using Gnome.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnome.Domain.Responses
+{
+    /// <summary>
+    /// Response with a single category for the FE
+    /// </summary>
+    [DataContract]
+    public class CategoryResponse
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        
+        [DataMember(Name = "slug")]
+        public string Slug { get; set; }
+        
+        [DataMember(Name = "created-date-time")]
+        public DateTime? CreatedDateTime { get; set; }
+        
+        [DataMember(Name = "products-count")]
+        public int ProductsCount { get; set; }
+    }
+} 
---
+++ Gnome.Domain/Responses/ProductResponse.cs
@@ -0,0 +1,42 @@
+using Gnome.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gnome.Domain.Responses
+{
+    /// <summary>
+    /// Response with a single product for the FE
+    /// </summary>
+    [DataContract]
+    public class ProductResponse
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+        
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+        
+        [DataMember(Name = "slug")]
+        public string Slug { get; set; }
+        
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+        
+        [DataMember(Name = "created-date-time")]
+        public DateTime? CreatedDateTime { get; set; }
+        
+        [DataMember(Name = "category-id")]
+        public int CategoryId { get; set; }
+        
+        [DataMember(Name = "category")]
+        public CategoryResponse Category { get; set; }
+        
+        [DataMember(Name = "variants")]
+        public List<VariantResponse> Variants { get; set; } = new();
+    }
+} 
